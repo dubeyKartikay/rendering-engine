5401aebede74cc6d1fbe02992982ca0a6ff54193
Kartikay Dubey <kartikayd2003@gmail.com>
2023-11-30 23:30:40 +0530

    OpenGL #4: Vertex Buffers and Drawing a Triangle in OpenGL

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -29,12 +29,25 @@ int main(void) {
   std::cout << glGetString(GL_VERSION) << std::endl;
   float positions[6] = {-0.5f, -0.5, 0.0f, 0.5f, 0.5f, -0.5f};
   unsigned int buffer_id;
+
+  //generate buffer
   glGenBuffers(1, &buffer_id);
+ 
+  //select buffer
   glBindBuffer(GL_ARRAY_BUFFER, buffer_id);
+
+  // fill buffer
   glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float),positions,GL_STATIC_DRAW);
+
+  //define buffer layout
+  glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE,2*sizeof(float),0);
+  glEnableVertexAttribArray(0);
+  
   while (!glfwWindowShouldClose(window)) {
     /* Render here */
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // draw selected buffer
     glDrawArrays(GL_TRIANGLES, 0, 3 );
     glfwSwapBuffers(window);
     glfwPollEvents();
