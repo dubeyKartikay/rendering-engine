0454c38623ff11093061e75963719eca05691376
Kartikay Dubey <kartikayd2003@gmail.com>
2023-12-04 16:08:34 +0530

    OpenGL #8:  Dealing with Errors in OpenGL

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -16,6 +16,113 @@
 #define VERT_ATTRIB_POSITON_NCOMP 2
 #define SHADER_ERROR_MESG_BUFF_SIZE 2048
 
+void APIENTRY GLDebugMessageCallback(GLenum source, GLenum type, GLuint id,
+                            GLenum severity, GLsizei length,
+                            const GLchar *msg, const void *data)
+{
+  std::string _source;
+    std::string _type;
+    std::string _severity;
+
+    switch (source) {
+        case GL_DEBUG_SOURCE_API:
+        _source = "API";
+        break;
+
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+        _source = "WINDOW SYSTEM";
+        break;
+
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:
+        _source = "SHADER COMPILER";
+        break;
+
+        case GL_DEBUG_SOURCE_THIRD_PARTY:
+        _source = "THIRD PARTY";
+        break;
+
+        case GL_DEBUG_SOURCE_APPLICATION:
+        _source = "APPLICATION";
+        break;
+
+        case GL_DEBUG_SOURCE_OTHER:
+        _source = "UNKNOWN";
+        break;
+
+        default:
+        _source = "UNKNOWN";
+        break;
+    }
+
+    switch (type) {
+        case GL_DEBUG_TYPE_ERROR:
+        _type = "ERROR";
+        break;
+
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+        _type = "DEPRECATED BEHAVIOR";
+        break;
+
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+        _type = "UDEFINED BEHAVIOR";
+        break;
+
+        case GL_DEBUG_TYPE_PORTABILITY:
+        _type = "PORTABILITY";
+        break;
+
+        case GL_DEBUG_TYPE_PERFORMANCE:
+        _type = "PERFORMANCE";
+        break;
+
+        case GL_DEBUG_TYPE_OTHER:
+        _type = "OTHER";
+        break;
+
+        case GL_DEBUG_TYPE_MARKER:
+        _type = "MARKER";
+        break;
+
+        default:
+        _type = "UNKNOWN";
+        break;
+    }
+
+    switch (severity) {
+        case GL_DEBUG_SEVERITY_HIGH:
+        _severity = "HIGH";
+        break;
+
+        case GL_DEBUG_SEVERITY_MEDIUM:
+        _severity = "MEDIUM";
+        break;
+
+        case GL_DEBUG_SEVERITY_LOW:
+        _severity = "LOW";
+        break;
+
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+        _severity = "NOTIFICATION";
+        break;
+
+        default:
+        _severity = "UNKNOWN";
+        break;
+    }
+    std::cout << id << ": "<< _type << " of " << _severity << " severity, raised from "<< _source << ": " << msg << std::endl;
+    exit(EXIT_FAILURE);
+}
+
+// =============== INIT DEBUG OUTPUT ================
+// The following function calls should be made directly after OpenGL
+// initialization.
+
+// Enable the debugging layer of OpenGL
+//
+// GL_DEBUG_OUTPUT - Faster version but not useful for breakpoints
+// GL_DEBUG_OUTPUT_SYNCHRONUS - Callback is in sync with errors, so a breakpoint
+// can be placed on the callback in order to get a stacktrace for the GL error.
+
 struct ShaderSource{
   std::string vertex_shader;
   std::string fragment_shader;
@@ -86,6 +193,7 @@ int main(void) {
   if (!glfwInit())
     return -1;
 
+	glfwWindowHint (GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
   /* Create a windowed mode window and its OpenGL context */
   window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
   if (!window) {
@@ -108,6 +216,8 @@ int main(void) {
   };
   unsigned int buffer_id;
   unsigned int index_buffer_id;
+  glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+  glDebugMessageCallback(GLDebugMessageCallback, NULL);
   // generate buffer
   glGenBuffers(1, &buffer_id);
   glGenBuffers(1,&index_buffer_id);
@@ -134,7 +244,7 @@ int main(void) {
     glClear(GL_COLOR_BUFFER_BIT);
 
     // draw selected buffer
-    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr); 
+    glDrawElements(GL_TRIANGLES, 6, GL_INT, nullptr); 
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
