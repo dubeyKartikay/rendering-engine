c2f57195a65394796a54004cedd06e4223b45ac9
Kartikay Dubey <kartikayd2003@gmail.com>
2023-12-19 19:37:14 +0530

    ImGUI

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -1,16 +1,19 @@
-#include "Texture.hpp"
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <GLUtils.hpp>
 #include <IndexBuffer.hpp>
+#include <Renderer.hpp>
 #include <Shaders.hpp>
+#include <Texture.hpp>
 #include <VertexArray.hpp>
 #include <VertexBuffer.hpp>
-#include <Renderer.hpp>
 #include <glm/ext/matrix_clip_space.hpp>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
-
+#include <iostream>
+#include <vendor/imgui/imgui.h>
+#include <vendor/imgui/imgui_impl_glfw.h>
+#include <vendor/imgui/imgui_impl_opengl3.h>
 #define VERT_ATTRIB_POSITON 0
 #define VERT_ATTRIB_POSITON_NCOMP 2
 #ifndef SHADER_DIR
@@ -31,7 +34,7 @@ int main(void) {
   glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
 
   /* Create a windowed mode window and its OpenGL context */
-  window = glfwCreateWindow(2560/2, 1440/2, "Hello World", NULL, NULL);
+  window = glfwCreateWindow(2560 / 2, 1440 / 2, "Hello World", NULL, NULL);
   if (!window) {
     std::cout << "Unable to create a window" << std::endl;
     const char *description;
@@ -39,28 +42,42 @@ int main(void) {
     std::cout << description << std::endl;
     return -1;
   }
-
-  /* Make the window's context current */
   glfwMakeContextCurrent(window);
   glfwSwapInterval(2);
   if (glewInit() != GLEW_OK) {
     std::cout << "GLEW INIT ERROR" << std::endl;
     exit(EXIT_FAILURE);
   }
+
+  // Setup Dear ImGui context
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
+  (void)io;
+  io.ConfigFlags |=
+      ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+  io.ConfigFlags |=
+      ImGuiConfigFlags_NavEnableGamepad; // Enable Gamepad Controls
+
+  // Setup Dear ImGui style
+  ImGui::StyleColorsDark();
+  // ImGui::StyleColorsLight();
+
+  // Setup Platform/Renderer backends
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init();
+
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
   std::cout << glGetString(GL_VERSION) << std::endl;
-  float positions[] = {-0.5f, -0.5f, 0.0f, 0.0f,
-                        0.5f, 0.5f, 1.0f, 1.0f,
-                        0.5f, -0.5f, 1.0f, 0.0,
-                        -0.5f, 0.5f, 0.0f, 1.0f
-  };
+  float positions[] = {-0.5f, -0.5f, 0.0f, 0.0f, 0.5f,  0.5f, 1.0f, 1.0f,
+                       0.5f,  -0.5f, 1.0f, 0.0,  -0.5f, 0.5f, 0.0f, 1.0f};
   unsigned int indices[] = {0, 1, 2, 0, 1, 3};
 
   glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
   glDebugMessageCallback(GLDebugMessageCallback, NULL);
 
-  VertexBuffer *vb = new VertexBuffer(positions, 4*4 * sizeof(float));
+  VertexBuffer *vb = new VertexBuffer(positions, 4 * 4 * sizeof(float));
   IndexBuffer *id = new IndexBuffer(indices, 6);
 
   VertexArray *va = new VertexArray();
@@ -68,25 +85,35 @@ int main(void) {
   va_layout->Push<float>(2);
   va_layout->Push<float>(2);
   va->AddBuffer(*vb, *va_layout);
-  
-  glm::mat4 proj = glm::ortho(-1.6f,1.6f,-0.9f,0.9f,-1.0f,1.0f);
 
-  Shader * shader = new Shader(SHADER_DIR "Basic.shader");
+  glm::mat4 proj = glm::ortho(-1.6f, 1.6f, -0.9f, 0.9f, -1.0f, 1.0f);
+
+  Shader *shader = new Shader(SHADER_DIR "Basic.shader");
   Renderer renderer;
-  
-  Texture * texture = new Texture(SHADER_DIR "background.png");
+
+  Texture *texture = new Texture(SHADER_DIR "background.png");
   texture->Bind();
   shader->Bind();
-  shader->setUniform1i("u_Texture",0);
-  shader->setUniformMat4f("u_MVP",proj);
+  shader->setUniform1i("u_Texture", 0);
+  shader->setUniformMat4f("u_MVP", proj);
   float r = 0;
   float inc = 0.05f;
   vb->Unbind();
   id->Unbind();
   va->Unbind();
   shader->Unbind();
+
+  bool show_demo_window = true;
+  bool show_another_window = false;
+  ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
   while (!glfwWindowShouldClose(window)) {
     renderer.Clear();
+
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
     if (r >= 1) {
       inc = -inc;
     }
@@ -94,13 +121,52 @@ int main(void) {
       inc = -inc;
     }
     r += inc;
+    {
+
+      static float f = 0.0f;
+      static int counter = 0;
+
+      ImGui::Begin("Hello, world!"); // Create a window called "Hello, world!"
+                                     // and append into it.
+
+      ImGui::Text("This is some useful text."); // Display some text (you can
+                                                // use a format strings too)
+      ImGui::Checkbox(
+          "Demo Window",
+          &show_demo_window); // Edit bools storing our window open/close state
+      ImGui::Checkbox("Another Window", &show_another_window);
+
+      ImGui::SliderFloat("float", &f, 0.0f,
+                         1.0f); // Edit 1 float using a slider from 0.0f to 1.0f
+      ImGui::ColorEdit3(
+          "clear color",
+          (float *)&clear_color); // Edit 3 floats representing a color
+
+      if (ImGui::Button("Button")) // Buttons return true when clicked (most
+                                   // widgets return true when edited/activated)
+        counter++;
+      ImGui::SameLine();
+      ImGui::Text("counter = %d", counter);
+
+      ImGui::Text("Application average %.3f ms/frame (%.1f FPS)",
+                  1000.0f / io.Framerate, io.Framerate);
+      ImGui::End();
+    }
     shader->Bind();
-//    shader->setUniform4f("u_Color", r, 0.102, 0.344, 1);
-    renderer.Draw(*va, *id, *shader);  
+    //    shader->setUniform4f("u_Color", r, 0.102, 0.344, 1);
+    renderer.Draw(*va, *id, *shader);
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
     glfwSwapBuffers(window);
     glfwPollEvents();
   }
+  ImGui_ImplOpenGL3_Shutdown();
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
   glfwTerminate();
+
   delete va;
   delete vb;
   delete id;
--- OpenGL/src/renderer/include/GLUtils.hpp
+++ OpenGL/src/renderer/include/GLUtils.hpp
@@ -2,8 +2,6 @@
 #define GL_UTILS
 #include <GL/glew.h>
 #include <GL/gl.h>
-#include <string>
-#include <iostream>
 void GLDebugMessageCallback(GLenum source, GLenum type, GLuint id,
                                      GLenum severity, GLsizei length,
                                      const GLchar *msg, const void *data);
