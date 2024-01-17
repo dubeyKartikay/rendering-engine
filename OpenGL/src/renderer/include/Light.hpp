41308381e40e676fd6318d23911cec8b4d033098
Kartikay Dubey <kartikayd2003@gmail.com>
2024-01-17 21:55:46 +0530

    House Keeping

---
+++ OpenGL/src/renderer/include/Light.hpp
@@ -0,0 +1,16 @@
+#ifndef LIGHT
+#define LIGHT
+#include <glm/glm.hpp>
+class Light{
+  private:
+
+  public:
+    Light();
+    ~Light();
+    void AddDirectionalLight(glm::vec3 normalisedDirection, float intensity);
+    void AddPointLight(glm::vec3 normalisedLightPosition, float intensity);
+    void AddSpotLight();
+};
+
+
+#endif // !LIGHT
