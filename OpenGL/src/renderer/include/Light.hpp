aa527d4ad2411a0cd90a70179bf074d1ef3cd309
Kartikay Dubey <kartikayd2003@gmail.com>
2024-02-12 17:45:38 +0530

    GODOT

--- OpenGL/src/renderer/include/Light.hpp
+++ OpenGL/src/renderer/include/Light.hpp
@@ -3,10 +3,21 @@
 #include <vector>
 #include <vendor/glm/glm.hpp>
 struct DirectionalLight{
+  glm::vec3 direction;
+  glm::vec3 ambient;
+  glm::vec3 diffuse;
+  glm::vec3 specular;
 
 };
 struct PointLight{
 
+  glm::vec3 position;
+  glm::vec3 ambient;
+  glm::vec3 diffuse;
+  glm::vec3 specular;
+  float constant;
+  float linear;
+  float quadratic;
 };
 
 // struct SpotLight{};
@@ -19,8 +30,13 @@ class Light{
 
     Light();
     ~Light();
-    void AddDirectionalLight(glm::vec3 normalisedDirection, float intensity);
-    void AddPointLight(glm::vec3 normalisedLightPosition, float intensity);
+    void AddDirectionalLight(glm::vec3 normalisedDirection, glm::vec3 diffuse);
+    void AddDirectionalLight(glm::vec3 normalisedDirection, glm::vec3 diffuse, glm::vec3 specular);
+    void AddDirectionalLight(glm::vec3 normalisedDirection, glm::vec3 diffuse, glm::vec3 specular, glm::vec3 ambient);
+    void AddPointLight(glm::vec3 normalisedLightPosition, glm::vec3 diffuse);
+    void AddPointLight(glm::vec3 normalisedLightPosition, glm::vec3 diffuse, glm::vec3 specular);
+    void AddPointLight(glm::vec3 normalisedLightPosition, glm::vec3 diffuse, glm::vec3 specular, glm::vec3 ambient);
+    void AddPointLight(glm::vec3 normalisedLightPosition, glm::vec3 diffuse, glm::vec3 specular, glm::vec3 ambient, float linear, float quadratic, float constant = 1.0f);
     // void AddSpotLight();
     const std::vector<DirectionalLight> & getDirectionalLights();
     const std::vector<PointLight> & getPointLights();
