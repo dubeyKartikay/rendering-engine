fe64df839510387681b36a360430562ea4f6d3d9
Kartikay Dubey <kartikayd2003@gmail.com>
2024-05-27 22:40:05 +0530

    update

---
+++ build/src/renderer/CMakeFiles/renderer.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/Camera.cpp" "src/renderer/CMakeFiles/renderer.dir/Camera.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/Camera.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/GLUtils.cpp" "src/renderer/CMakeFiles/renderer.dir/GLUtils.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/GLUtils.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/IndexBuffer.cpp" "src/renderer/CMakeFiles/renderer.dir/IndexBuffer.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/IndexBuffer.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/Input.cpp" "src/renderer/CMakeFiles/renderer.dir/Input.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/Input.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/Light.cpp" "src/renderer/CMakeFiles/renderer.dir/Light.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/Light.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/Mesh.cpp" "src/renderer/CMakeFiles/renderer.dir/Mesh.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/Mesh.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/Model.cpp" "src/renderer/CMakeFiles/renderer.dir/Model.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/Model.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/Renderer.cpp" "src/renderer/CMakeFiles/renderer.dir/Renderer.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/Renderer.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/Shaders.cpp" "src/renderer/CMakeFiles/renderer.dir/Shaders.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/Shaders.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/Texture2D.cpp" "src/renderer/CMakeFiles/renderer.dir/Texture2D.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/Texture2D.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/VertexArray.cpp" "src/renderer/CMakeFiles/renderer.dir/VertexArray.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/VertexArray.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/VertexBuffer.cpp" "src/renderer/CMakeFiles/renderer.dir/VertexBuffer.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/VertexBuffer.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/VertexBufferLayout.cpp" "src/renderer/CMakeFiles/renderer.dir/VertexBufferLayout.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/VertexBufferLayout.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/include/vendor/imgui/imgui.cpp" "src/renderer/CMakeFiles/renderer.dir/include/vendor/imgui/imgui.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/include/vendor/imgui/imgui.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/include/vendor/imgui/imgui_demo.cpp" "src/renderer/CMakeFiles/renderer.dir/include/vendor/imgui/imgui_demo.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/include/vendor/imgui/imgui_demo.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/include/vendor/imgui/imgui_draw.cpp" "src/renderer/CMakeFiles/renderer.dir/include/vendor/imgui/imgui_draw.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/include/vendor/imgui/imgui_draw.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/include/vendor/imgui/imgui_impl_glfw.cpp" "src/renderer/CMakeFiles/renderer.dir/include/vendor/imgui/imgui_impl_glfw.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/include/vendor/imgui/imgui_impl_glfw.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/include/vendor/imgui/imgui_impl_opengl3.cpp" "src/renderer/CMakeFiles/renderer.dir/include/vendor/imgui/imgui_impl_opengl3.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/include/vendor/imgui/imgui_impl_opengl3.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/include/vendor/imgui/imgui_tables.cpp" "src/renderer/CMakeFiles/renderer.dir/include/vendor/imgui/imgui_tables.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/include/vendor/imgui/imgui_tables.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/include/vendor/imgui/imgui_widgets.cpp" "src/renderer/CMakeFiles/renderer.dir/include/vendor/imgui/imgui_widgets.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/include/vendor/imgui/imgui_widgets.cpp.o.d"
+  "/home/kartikayd/learning-repos/OpenGL/src/renderer/include/vendor/stb/stb_image.cpp" "src/renderer/CMakeFiles/renderer.dir/include/vendor/stb/stb_image.cpp.o" "gcc" "src/renderer/CMakeFiles/renderer.dir/include/vendor/stb/stb_image.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/main.cpp
@@ -0,0 +1,130 @@
+#include <GLUtils.hpp>
+#include "Input.hpp"
+#include "TestLighting.hpp"
+#include "TestModel3D.hpp"
+#include <GLFW/glfw3.h>
+#include <IndexBuffer.hpp>
+#include <Renderer.hpp>
+#include <Shaders.hpp>
+#include <TestCamera3D.hpp>
+#include <TestCube3D.hpp>
+#include <TestFrame.hpp>
+#include <TestRect2D.hpp>
+#include <TestTexture2D.hpp>
+#include <TestTexture3D.hpp>
+#include <Texture2D.hpp>
+#include <VertexArray.hpp>
+#include <VertexBuffer.hpp>
+#include <vendor/glm/ext/matrix_clip_space.hpp>
+#include <vendor/glm/glm.hpp>
+#include <vendor/glm/gtc/matrix_transform.hpp>
+#include <iostream>
+#include <vendor/imgui/imgui.h>
+#include <vendor/imgui/imgui_impl_glfw.h>
+#include <vendor/imgui/imgui_impl_opengl3.h>
+
+#define VERT_ATTRIB_POSITON 0
+#define VERT_ATTRIB_POSITON_NCOMP 2
+#ifndef SHADER_DIR
+#define SHADER_DIR 0
+#endif
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+int main(void) {
+  GLFWwindow *window;
+  std::cout << "Started program" << std::endl;
+  /* Initialize the library */
+  if (!glfwInit())
+    return -1;
+
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+
+  /* Create a windowed mode window and its OpenGL context */
+  window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+  if (!window) {
+    std::cout << "Unable to create a window" << std::endl;
+    const char *description;
+    int code = glfwGetError(&description);
+    std::cout << description << std::endl;
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+  if (glewInit() != GLEW_OK) {
+    std::cout << "GLEW INIT ERROR" << std::endl;
+    exit(EXIT_FAILURE);
+  }
+
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
+  (void)io;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
+
+  ImGui::StyleColorsDark();
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init();
+
+  glEnable(GL_BLEND);
+  glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+  glDebugMessageCallback(GLDebugMessageCallback, NULL);
+  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+  std::cout << glGetString(GL_VERSION) << std::endl;
+
+  Input::Initialize(window);
+
+  TestFrame tests;
+  // tests.AddTest(new TestCube3D());
+  // tests.AddTest(new TestRect2D());
+  // tests.AddTest(new TestTexture2D());
+  // tests.AddTest(new TestCamera3D());
+  // tests.AddTest(new TestModel3D());
+  tests.AddTest(new TestLighting());
+
+  float deltaTime = 0.0f;
+  float lastFrame = 0.0f;
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+    if (Input::GetKeyPressed('`') && Input::GetKeyPressed('$')) {
+      glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+      glfwSetInputMode(window, GLFW_RAW_MOUSE_MOTION, GLFW_TRUE);
+    } else if (Input::GetKeyPressed('`')) {
+      glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+    }
+
+    float currFrame = glfwGetTime();
+    deltaTime = currFrame - lastFrame;
+    lastFrame = currFrame;
+
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    tests.Clear();
+    tests.Update(deltaTime);
+    tests.Render();
+    tests.ImGuiRender();
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+  ImGui_ImplOpenGL3_Shutdown(;
+  ImGui_ImplGlfw_Shutdown();
+  ImGui::DestroyContext();
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return 0;
+}
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  // make sure the viewport matches the new window dimensions; note that width
+  // and height will be significantly larger than specified on retina displays.
+  glViewport(0, 0, width, height);
+}
